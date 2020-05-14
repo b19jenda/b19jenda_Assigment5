cf2e478d8688295f4de4e5b1f8d0760c8d72a759
b19jenda <b19jenda@student.his.se>
2020-05-14 20:34:52 +0200

    La till en toast på min about-knapp

--- app/src/main/java/com/example/assigment5/MainActivity.java
+++ app/src/main/java/com/example/assigment5/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.assigment5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.about){
+            Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
