dc17de437efb67c8249b383f5f93f7f00d34450a
b19jenda <b19jenda@student.his.se>
2020-05-18 19:15:54 +0200

    Skapa arraylist och arrayadapter och kopplat adaptern till listviewn

--- app/src/main/java/com/example/assigment5/MainActivity.java
+++ app/src/main/java/com/example/assigment5/MainActivity.java
@@ -6,14 +6,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private String[]items={"1" ,"2" ,"3"};
+    private ArrayList<String> Listdata=new ArrayList<>(Arrays.asList(items));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.lista_textview, R.id.lista_textview_xml,Listdata);
+        ListView lista=(ListView) findViewById(R.id.lista);
+        lista.setAdapter(adapter);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
