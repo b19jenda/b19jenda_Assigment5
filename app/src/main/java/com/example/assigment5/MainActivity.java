82b31f5b27f8094780f893f5a04030882ccb843c
b19jenda <b19jenda@student.his.se>
2020-05-22 23:55:25 +0200

    la till JSON task klass i main activity och hämtade data från URLen och skickade till klassen Painters

--- app/src/main/java/com/example/assigment5/MainActivity.java
+++ app/src/main/java/com/example/assigment5/MainActivity.java
@@ -2,27 +2,50 @@ package com.example.assigment5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private String[]items={"1" ,"2" ,"3"};
-    private ArrayList<String> Listdata=new ArrayList<>(Arrays.asList(items));
+    private ArrayList<Painters> Listdata=new ArrayList<>();
+    ArrayAdapter<Painters> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.lista_textview, R.id.lista_textview_xml,Listdata);
-        ListView lista=(ListView) findViewById(R.id.lista);
+        adapter=new ArrayAdapter<Painters>(this,R.layout.lista_textview, R.id.lista_textview_xml,Listdata);
+        final ListView lista=(ListView) findViewById(R.id.lista);
         lista.setAdapter(adapter);
+        new JsonTask().execute();
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),Listdata.get(position).info(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,5 +61,70 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL("https://wwwlab.iit.his.se/b19jenda/project.php");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("hej","Data:"+s);
+            try {
+                JSONArray json1 = new JSONArray(s);
+                //Log.d("hej","Get:"+json1);
+                for(int i=0; i < json1.length() ; i++) {
+                    JSONObject json_data = json1.getJSONObject(i);
+                    String name=json_data.getString("name");
+                    String location=json_data.getString("location");
+                    String category=json_data.getString("category");
+                    int birth=json_data.getInt("company");
+                    int death=json_data.getInt("cost");
+                    Log.d("hej2",name);
+                    Listdata.add(new Painters(name,location,category,birth, death));
+                }
+                adapter.notifyDataSetChanged();
+
+            } catch (JSONException e) {
+                Log.e("brom","E:"+e.getMessage());
+            }
+        }
     }
 }
