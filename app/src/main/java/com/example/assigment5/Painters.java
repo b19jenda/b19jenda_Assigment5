4bb4b4d09df28e02923a7ed1af1915086a82bb6e
b19jenda <b19jenda@student.his.se>
2020-05-22 23:31:49 +0200

    skapade en klass och skapade variablerna och en konstruktor

---
+++ app/src/main/java/com/example/assigment5/Painters.java
@@ -0,0 +1,19 @@
+package com.example.assigment5;
+
+public class Painters {
+    private String name;
+    private String location;
+    private String category;
+    private int birthdate;
+    private int deathdate;
+
+    public Painters(String n, String l, String c, int b, int d){
+        name=n;
+        location=l;
+        category=c;
+        birthdate=b;
+        deathdate=d;
+    }
+
+}
+
